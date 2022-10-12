aa192cea5d98b03d10c232454c05d88d0980e442
valentinolie24 <valenlie2409@gmail.com>
2022-10-12 17:07:24 +0700

    update 1

--- app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
@@ -2,13 +2,63 @@ package com.si5a.implicitsi5a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etURL, etLokasi, etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etURL = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_teks);
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlWebsite = etURL.getText().toString();
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intentWebsite);
+                }
+                catch (Exception e){
+                    etURL.setError("Url Tidak Sesuai!");
+                }
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String lokasi = etLokasi.getText().toString();
+                Uri uriLokasi = Uri.parse(lokasi);
+                Intent intentLokasi = new Intent(Intent.ACTION_VIEW, uriLokasi);
+                startActivity(intentLokasi);
+                etLokasi.setError("Lokasi Harus Diisi!");
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teks = etTeks.getText().toString();
+                Uri uriTeks = Uri.parse(teks);
+                Intent intentTeks = new Intent(Intent.ACTION_VIEW, uriTeks);
+                startActivity(intentTeks);
+            }
+        });
     }
 }
